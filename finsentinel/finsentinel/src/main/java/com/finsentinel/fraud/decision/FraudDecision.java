d1ccbd50f66f337619eb38fb74ebf11b0d49d805
Shashank-Daga <shashankdaga2510@gmail.com>
2026-05-05 02:59:26 +0530

    Issue Fix 1.0

--- finsentinel/finsentinel/src/main/java/com/finsentinel/fraud/decision/FraudDecision.java
+++ finsentinel/finsentinel/src/main/java/com/finsentinel/fraud/decision/FraudDecision.java
@@ -2,17 +2,15 @@ package com.finsentinel.fraud.decision;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @AllArgsConstructor
 public class FraudDecision {
+    @Getter
     private boolean fraud;
     private String status;
     private String type;
     private String reason;
     private double confidence;
-
-    public boolean isFraud() {
-        return fraud;
-    }
 }
